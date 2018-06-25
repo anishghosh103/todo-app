1719e1e0a290fd1614c1abf19e7adaae448374a4
Anish Ghosh <anishghosh103@gmail.com>
2018-06-25 09:04:36 +0530

    API Documentaion URL change

--- server/doc/api_project.js
+++ server/doc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "1.0.0",
   "description": "Documentation for an API of a Todo App",
   "title": "Todo App API Documentation",
-  "url": "https://localhost:3000",
+  "url": "todo.anishghosh.me",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-24T15:09:36.705Z",
+    "time": "2018-06-25T03:33:00.261Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
